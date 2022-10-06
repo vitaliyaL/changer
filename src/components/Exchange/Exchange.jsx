310b4e0f9003f2692e3aa685b6bbca11d60a25a9
vitaliyaL <vita0623@mail.ru>
2022-10-07 01:16:02 +0300

    feat: started converter

--- src/components/Exchange/Exchange.jsx
+++ src/components/Exchange/Exchange.jsx
@@ -7,9 +7,6 @@ import Grid from "@mui/material/Unstable_Grid2";
 import { styled } from "@mui/material/styles";
 import InputArea from "../InputArea/InputArea";
 import CurrencyTable from "../CurrencyTable/CurrencyTable";
-import { useSelector } from "react-redux";
-import { useEffect } from "react";
-import { useState } from "react";
 const Item = styled(Paper)(({ theme }) => ({
   padding: theme.spacing(2),
   textAlign: "center",
@@ -17,29 +14,18 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 function Exchange() {
-  const [num, setNum] = useState(0);
-  const { table, loading, error } = useSelector((state) => state.table);
-  useEffect(() => {
-    setNum(table.filter((j) => j?.CoinInfo?.Name === "BTC")[0]?.RAW?.USD.PRICE);
-  }, [loading, num]);
-  console.log(num);
-  const handleChange = (e) => {
-    console.log(e.target.value);
-  };
   return (
     <Container maxWidth='lg' className='exchange_container'>
       <Grid container spacing={2}>
         <Grid xs={7}>
           <Item elevation={2} className='left_exchange_container'>
-            <CurrencyTable />
+            <CurrencyTable/>
           </Item>
         </Grid>
         <Grid xs={5}>
           <Item elevation={2}>
-            <InputArea defaultCur={"BTC"} defaultCurVal={1} />
-            <InputArea defaultCurVal={num} defaultCur={"USDT"} />
-            {/* <input type="text"  defaultValue={num} /> */}
-            <p>{num}</p>
+            <InputArea defaultCur={"BTC1"} defaultCurVal={0} idVal={"firstInput"} idCur={"firstCur"} num={1}/>
+            <InputArea defaultCurVal={0} defaultCur={"USDT2"} idVal={"secondInput"} idCur={"secondCur"} num={2}/>
           </Item>
         </Grid>
       </Grid>
--- src/components/InputArea/InputArea.jsx
+++ src/components/InputArea/InputArea.jsx
@@ -1,53 +1,89 @@
-import React from 'react'
-import "./InputArea.css"
+import React from "react";
+import "./InputArea.css";
 import TextField from "@mui/material/TextField";
 import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
-import InputLabel from '@mui/material/InputLabel';
-import {useState} from 'react'
-function InputArea({defaultCurVal, defaultCur}) {
-    const [currency, setCurrency] = useState(defaultCur);
-    const [currencyVal, setCurrencyVal] = useState(defaultCurVal);
+import InputLabel from "@mui/material/InputLabel";
+import { useState } from "react";
+import { array_coins } from "../../constants/array_coins";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+function InputArea({ defaultCurVal, defaultCur, idVal, idCur, num }) {
+  const [currency, setCurrency] = useState(defaultCur);
+  const [firstCur, setFirstCur] = useState("BTC");
+  const [secondCur, setSecondCur] = useState("USDT");
+  // let firstCur="BTC"
+  // let secondCur="USDT"
+  const firstInp = document.getElementById("firstInput");
+  const secondInp = document.getElementById("secondInput");
+  // const firstCur=document.getElementById("firstCur")
+  // const secondCur=document.getElementById("secondCur")
+  const { table } = useSelector((state) => state.table);
+  
+
   const handleChange = (event) => {
     setCurrency(event.target.value);
+    console.log(currency);
+    console.log(event.target.value[event.target.value.length - 1]);
+    if (event.target.value[event.target.value.length - 1] === "1") {
+      setFirstCur(event.target.value.slice(0, event.target.value.length - 1));
+      console.log(firstCur);
+    } else
+      setSecondCur(event.target.value.slice(0, event.target.value.length - 1));
+    console.log(firstCur, secondCur);
   };
-  const handleChangeTxt=(e)=>{
-    
-  }
+
+  const handleChangeTxt = (e) => {
+    if (e.target.id === "firstInput") {
+      secondInp.value =
+        (e.target.value *
+          table.filter((j) => j?.CoinInfo?.Name === firstCur)[0]?.RAW?.USD
+            .PRICE) /
+        table.filter((j) => j?.CoinInfo?.Name === secondCur)[0]?.RAW?.USD.PRICE;
+        console.log(table.filter((j) => j?.CoinInfo?.Name === firstCur)[0]?.RAW?.USD
+        .PRICE
+  );
+        console.log(table.filter((j) => j?.CoinInfo?.Name === secondCur)[0]?.RAW?.USD
+            .PRICE
+      );
+      console.log(e.target.value);
+      console.log(
+        table.filter((j) => j?.CoinInfo?.Name === firstCur)[0]?.RAW?.USD.PRICE
+      );
+    } else {
+    }
+  };
+
   return (
-         <div className='input_area'>
-              <TextField fullWidth label="Сумма" value={currencyVal} onChange={handleChangeTxt}/>
-              <FormControl fullWidth>
-                <InputLabel id='demo-simple-select-label'>Валюта</InputLabel>
-                <Select value={currency} label='Валюта' onChange={handleChange}>
-                  <MenuItem value={"BTC"} >BTC</MenuItem>
-                  <MenuItem value={"ETH"} >ETH</MenuItem>
-                  <MenuItem value={"USDT"} >USDT</MenuItem>
-                  <MenuItem value={"USDC"}>USDC</MenuItem>
-                  <MenuItem value={"BNB"}>BNB</MenuItem>
-                  <MenuItem value={"XRP"}>XRP</MenuItem>
-                  <MenuItem value={"ADA"}>ADA</MenuItem>
-                  <MenuItem value={"SOL"}>SOL</MenuItem>
-                  <MenuItem value={"DOGE"}>DOGE</MenuItem>
-                  <MenuItem value={"DOT"}>DOT</MenuItem>
-                  <MenuItem value={"MATIC"}>MATIC</MenuItem>
-                  <MenuItem value={"TRX"}>TRX</MenuItem>
-                  <MenuItem value={"UNI"}>UNI</MenuItem>
-                  <MenuItem value={"LTC"}>LTC</MenuItem>
-                  <MenuItem value={"ETC"}>ETC</MenuItem>
-                  <MenuItem value={"LINK"}>LINK</MenuItem>
-                  <MenuItem value={"XLM"}>XLM</MenuItem>
-                  <MenuItem value={"XMR"}>XMR</MenuItem>
-                  <MenuItem value={"BCH"}>BCH</MenuItem>
-                  <MenuItem value={"EOS"}>EOS</MenuItem>
-                  <MenuItem value={"ZEC"}>ZEC</MenuItem>
-                  <MenuItem value={"NEO"}>NEO</MenuItem>
-                  <MenuItem value={"DASH"}>DASH</MenuItem>
-                </Select>
-              </FormControl>
-            </div>
-  )
+    <div className='input_area'>
+      <TextField
+        fullWidth
+        id={idVal}
+        label='Сумма'
+        defaultValue={defaultCurVal}
+        onInput={handleChangeTxt}
+      />
+      <FormControl fullWidth>
+        <InputLabel id={`${idCur}-label`}>Валюта</InputLabel>
+        <Select
+          value={currency}
+          id={idCur}
+          labelId={`${idCur}-label`}
+          label='Валюта'
+          onChange={handleChange}
+        >
+          {array_coins.map((i, index) => {
+            return (
+              <MenuItem key={index} value={`${i}${num}`}>
+                {i}
+              </MenuItem>
+            );
+          })}
+        </Select>
+      </FormControl>
+    </div>
+  );
 }
 
-export default InputArea
+export default InputArea;
